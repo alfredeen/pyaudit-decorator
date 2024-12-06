0ca7cf276f51869090d2215d252a3677f0fc1302
alfredeen <johan.alfredeen@gmail.com>
2024-12-06 14:25:53 +0100

    Started working on the auditor decorator.

--- src/auditor.py
+++ src/auditor.py
@@ -1,9 +1,24 @@
-def simple_decorator(func):
-    """A very simple decorator for testing."""
+def audit_decorator(func, context=None):
+    """
+    An audit decorator.
+
+    TODO: Add facilities to:
+    - capture and append information of interest:
+    Pattern: [ts] [userid] [action-verb] [object-type [objectid] [view/endpoint]
+    Example AUDIT: ts user102 created user-app 1234 view/apps/create
+    Example AUDIT: ts user101 updated user-profile None view/edit-profile
+    """
 
     def wrapper(*args, **kwargs):
-        print("simple_decorator:Before function call")
         func(*args)
-        # print("After the function call")
+
+        # Parse the context. Expected data for now: context["classname"]
+        if context:
+            classname = context["classname"]
+
+        logmsg = f"test {classname} {func}"
+
+        # TODO: add logging
+        print(logmsg)
 
     return wrapper
---
+++ tests/test_auditor.py
@@ -0,0 +1,6 @@
+# from .sample_app import simple_view
+
+
+def test_verify_auditor_access():
+    """A simple test to verify the test setup."""
+    assert True
